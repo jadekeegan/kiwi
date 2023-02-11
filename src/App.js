16066513e23c9751cf26b5b02d298b991e015fa9
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-02-11 18:15:34 -0500

    home page and nav bar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home.component";
 import SignUp from "./components/signup.component";
 import SignIn from "./components/signin.component";
+import Navbar from "./components/home-navbar.component"
 
 function App() {
   return (
    <BrowserRouter>
+    <Navbar />
     <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
     </Routes>
- </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
