b2575b4018beb9fbef003e70be0574c4f4b3fc6e
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-02-11 14:58:45 -0500

    added sign up html

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -4,9 +4,19 @@ export default class SignUp extends Component {
 
   render() {
     return (
-      <div>
+      <div class="sign-up">
         <h1>Sign Up</h1>
+        <form onSubmit={this.handleSubmit}>
+          
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+
       </div>
     );
-  }
+  } 
 }
