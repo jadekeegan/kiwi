3182bd342ae71d41f8117a6c2bc7e11a81363b69
Brianna Ta <37271052+briannata@users.noreply.github.com>
2023-02-11 14:43:56 -0500

    made sign in and sign up components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home.component";
+import SignUp from "./components/signup.component";
+import SignIn from "./components/signin.component";
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
     </Routes>
  </BrowserRouter>
   );
---
+++ src/components/signin.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SignIn extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Sign In</h1>
+      </div>
+    );
+  }
+}
