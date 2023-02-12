32912850be8e520d8b0d027b9496f0c8afaf5253
Adela Zhang <azhang2@unc.edu>
2023-02-11 20:06:57 -0500

    new sign in styling & added isLoggedIn

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import SignUp from "./components/signup.component";
 import SignIn from "./components/signin.component";
 import Navbar from "./components/home-navbar.component"
 import Dashboard from "./components/dashboard.component";
+import { useState, React } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
    <BrowserRouter>
     <Navbar />
     <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-in" element={<SignIn setIsLoggedIn={setIsLoggedIn}/>}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
     </Routes>
     </BrowserRouter>
--- src/components/signin.component.js
+++ src/components/signin.component.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Dashboard from './dashboard.component';
 import "./signin.component.css"
 
-export default class SignIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
-    };
+const SignIn = (setIsLoggedIn) => {
+  
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleInputChange(event) {
+  const handleInputChange = (event) => {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
@@ -24,30 +16,31 @@ export default class SignIn extends Component {
     });
   }
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
+    setIsLoggedIn(true)
     event.preventDefault();
   }
 
-  render() {
     return (
       <div class="signin-page">
-        <h1>Sign In</h1>
-        <form onSubmit={this.handleSubmit}>
-        <label>
-          Username: 
-          <input name="username" type="text" placeholder="username or email" value={this.state.value} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Password: 
-          <input name="password" type="text" value={this.state.value} onChange={this.handleInputChange} />
-        </label>
-        <br>
-        </br>
-          <Link to="/dashboard">
-            <input type="submit" value="Submit" />
-          </Link>
-        </form>
+        <p class="border">
+          <h1>welcome back!</h1>
+          <p>please enter your details:</p>
+          <form onSubmit={handleSubmit}>
+            <label> 
+              <input name="username" type="text" placeholder="username or email" onChange={handleInputChange} />
+            </label>
+            <label>
+              <input name="password" type="text" placeholder="password" onChange={handleInputChange} />
+            </label>
+            <Link to="/dashboard">
+              <input type="submit" value="log in!" />
+            </Link>
+            <p class="body">don't have an account? <a class="body" href="/sign-up">sign up here!</a></p>
+         </form>
+        </p>
       </div>
     );
   }
-}
+
+  export default SignIn;
