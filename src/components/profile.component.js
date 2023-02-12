cb56e15ae4e9cf45a50cc328e848a31567a45ed6
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-02-12 01:29:42 -0500

    styled

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -4,22 +4,22 @@ import "./profile.component.css"
 export default class Profile extends Component {
   render() {
     return (
-      <div class="profile-page">
-        <h1>your kiwi profile 🥝</h1>
-        <p class="section"> 
-        <h2>personal settings </h2> 
+      <div className="profile-page">
+        <h1 className="profile-title">your kiwi profile 🥝</h1>
+        <p className="section"> 
+        <h2 className="profile-subtitle">personal settings </h2> 
           <p>email: </p>
           <p>phone number: </p>
-          <button class="edit-button"> edit </button>
+          <button className="edit-button"> edit </button>
         </p>
         <br></br>
-        <p class="section">
+        <p className="section">
           <h2>budget settings</h2>
           <p>savings per month: </p>
           <p>expected monthly income: </p>
           <p>fixed monthly expenses: </p>
           <p>expenses per category:</p> 
-          <button class="edit-button"> edit </button>
+          <button className="edit-button"> edit </button>
         </p>
       </div>
     );
