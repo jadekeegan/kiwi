5378acdb134e166c1994e9e48fd045317894995c
tdianaa <31745478+atoneyd@users.noreply.github.com>
2023-02-11 22:19:04 -0500

    added table

--- src/components/dashboard.component.js
+++ src/components/dashboard.component.js
@@ -1,9 +1,26 @@
 import { useState, React } from 'react';
+import DataTable from 'react-data-table-component';
 import axios from 'axios'
+let tableData = []
 
 const Dashboard = () => {
 	const [selectedFile, setSelectedFile] = useState();
 	const [isSelected, setIsSelected] = useState(false);
+
+  const columns = [
+    {
+        name: 'Date',
+        selector: row => row.date,
+    },
+    {
+        name: 'Purchase',
+        selector: row => row.merchantName,
+    },
+    {
+      name: 'Amount',
+      selector: row => row.totalAmount,
+  },
+];
 
 	const handleUpload = (event) => {
 		setSelectedFile(event.target.files[0]);
@@ -25,7 +42,14 @@ const Dashboard = () => {
           'apikey': "2bc787c0aa6c11edae52ab17ace764b5"
         } 
       })
-      console.log(res)
+      console.log(res.data)
+      tableData.push({
+        date: res.data.date.data ?? "N/A",
+        merchantName: res.data.merchantName.data,
+        totalAmount: res.data.totalAmount.data
+      })
+      console.log(tableData)
+      setIsSelected(false);
     } catch (e) {
       console.error(e);
     }
@@ -35,6 +59,7 @@ const Dashboard = () => {
 		<div>
 			<h1>Dashboard</h1>
 			<div>
+      <h2>File Upload</h2>
 				<input type="file" name="file" onChange={handleUpload} />
 				{isSelected ? (
 					<div>
@@ -53,6 +78,11 @@ const Dashboard = () => {
 					<button onClick={handleSubmit}>Submit</button>
 				</div>
 			</div>
+      <h2>Expenses</h2>
+      <DataTable
+            columns={columns}
+            data={tableData}
+        />
 		</div>
 	)
 }
