994a79640c378f46624ed960304463612e5e2825
Adela Zhang <azhang2@unc.edu>
2023-02-11 22:39:55 -0500

    attempted work on profile

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+
 export default class Profile extends Component {
   render() {
     return (
       <div>
         <h1>Profile</h1>
+        <p>Email: </p>
       </div>
     );
   }
