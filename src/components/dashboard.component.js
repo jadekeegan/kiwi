8cca3eda9a94e01a9bcd092ae279589a342b8769
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-02-12 02:19:29 -0500

    dashboard extra analytics

--- src/components/dashboard.component.js
+++ src/components/dashboard.component.js
@@ -3,6 +3,7 @@ import DataTable from 'react-data-table-component';
 import axios from 'axios'
 
 import "./dashboard.component.css"
+import chart from "./sample-chart.png";
 
 import CanvasJSReact from '../assets/canvasjs.react';
 var CanvasJS = CanvasJSReact.CanvasJS;
@@ -32,13 +33,17 @@ const Dashboard = () => {
 
 const options = {
   title: {
-    text: "Yearly Expense Summary"
+    text: "yearly expense summary",
+    fontColor: "#1a312b",
+    fontFamily: "Raleway",
+    fontWeight: "bold"
   },
   data: [{				
             type: "column",
             axisX:{
+              labelFontFamily: "Raleway",
               interval: 1,
-   intervalType: "month",
+              intervalType: "month"
             },
             dataPoints: [
                 { x: new Date(2022, 0),  y: chartData[0]  },
@@ -54,9 +59,9 @@ const options = {
                 { x: new Date(2022, 10),  y: chartData[10]  },
                 { x: new Date(2022, 11),  y: chartData[11]  },
             ]
-   }]
+   }],
+   backgroundColor: "#F5F3F0"
 }
-console.log(chartData)
 
 	const handleUpload = (event) => {
 		setSelectedFile(event.target.files[0]);
@@ -78,7 +83,6 @@ console.log(chartData)
           'apikey': "2bc787c0aa6c11edae52ab17ace764b5"
         } 
       })
-      console.log(res.data)
       let date = new Date(Date.parse(res.data.date.data))
       chartData[date.getMonth()] += res.data.totalAmount.data
       tableData.push({
@@ -86,34 +90,19 @@ console.log(chartData)
         merchantName: res.data.merchantName.data,
         totalAmount: res.data.totalAmount.data
       })
-      console.log(tableData)
       setIsSelected(false);
     } catch (e) {
-      console.error(e);
     }
 	};
 
 	return (
 		<div className="dashboard-container">
-			<h1>Dashboard</h1>
+			<h1>dashboard</h1>
 			<section id="file-upload-section">
         <div>
-          <h2>File Upload</h2>
+          <h2>file upload</h2>
           <div class="file-upload">
             <input type="file" name="file" onChange={handleUpload} />
-            {isSelected ? (
-              <div>
-                <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
-                <p>Size in bytes: {selectedFile.size}</p>
-                <p>
-                  lastModifiedDate:{' '}
-                  {selectedFile.lastModifiedDate.toLocaleDateString()}
-                </p>
-              </div>
-            ) : (
-              <p>Select a file to show details</p>
-            )}
           </div>
             <div>
               <button class="submit-button" onClick={handleSubmit}>Submit</button>
@@ -123,14 +112,18 @@ console.log(chartData)
 			
 
       <section id="expenses-section">
-      <h2>Expenses</h2>
+      <h2>expenses</h2>
       <DataTable className="data-table"
             columns={columns}
             data={tableData}
         />
       </section>
-      <CanvasJSChart options = {options}
-        />
+
+      <section id="analytics-section">
+        <h2>analytics</h2>
+        <CanvasJSChart options = {options}/>
+      </section>
+      <img src={chart}/>
     </div>
 	)
 }
