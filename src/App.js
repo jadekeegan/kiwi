17693d6d2044e56aaf67e7273111fe4174a44328
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-02-11 19:57:11 -0500

    created private nav bar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home.component";
 import SignUp from "./components/signup.component";
 import SignIn from "./components/signin.component";
-import Navbar from "./components/home-navbar.component"
 import Dashboard from "./components/dashboard.component";
+import Profile from "./components/profile.component";
+
+import PrivateNav from "./components/private-navbar.component";
+import Navbar from "./components/home-navbar.component";
 
 function App() {
   return (
    <BrowserRouter>
-    <Navbar />
+    <PrivateNav />
+
     <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
     </Routes>
     </BrowserRouter>
   );
--- src/components/dashboard-navbar.component.js
+++ src/components/dashboard-navbar.component.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "./home-navbar.component.css"
 
-const Navbar = () => {
-  const location = useLocation();
-
+const DashboardNav = () => {
   return (
     <div class="home-navbar">
       <ul>
@@ -17,14 +15,12 @@ const Navbar = () => {
             </div>
           </Link>
         </li>
-      
-        { location.pathname !=="/sign-in" && 
+        
         <li>
           <Link to="/sign-in" class="nav-link">
             <button class="nav-button">sign in</button>
           </Link>
         </li>
-        }
         
       </ul>
         
