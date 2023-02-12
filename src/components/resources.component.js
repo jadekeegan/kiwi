0064dd1ef8efe9028ad13279b2b2efefbcf5c20c
Adela Zhang <azhang2@unc.edu>
2023-02-12 10:14:12 -0500

    resources page completed

--- src/components/resources.component.js
+++ src/components/resources.component.js
@@ -6,9 +6,27 @@ import "./resources.component.css"
 export default class Resources extends Component {
 render() {
     return (
-        <div className='resource-page'>
+        <div className='slide-up'>
             <div>
-                <h1></h1>
+                <h1>extra resources for your financial journey!</h1>
+                <p>
+                    read a complete guide with <span> </span>
+                    <a href="https://www.investopedia.com/personal-finance-4427760/">
+                        investopedia
+                    </a>
+                </p>
+                <p>
+                    learn the basics lesson-by-lesson using<span> </span>
+                    <a href="https://www.khanacademy.org/college-careers-more/personal-finance/">
+                        khan academy
+                    </a>
+                </p>
+                <p>
+                    follow current financial news at<span> </span>
+                    <a href="https://www.cnbc.com/personal-finance/">
+                        cnbc
+                    </a>
+                </p>
             </div>
         </div>
     )
