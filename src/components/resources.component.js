f13a358c2481e3e651d7a75cd2a5428caf77d933
Adela Zhang <azhang2@unc.edu>
2023-02-12 09:36:08 -0500

    resources component

---
+++ src/components/resources.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+  
+  export default class Resources extends Component {
+    render() {
+        return (
+            <div>
+              <p>
+                  <a href="https://herewecode.io/">
+                      Click to open HereWeCode (current tab)
+                  </a>
+              </p>
+            </div>
+        )
+    }
+  }
